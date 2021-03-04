022ab0d31a8e208cbfec48f0eff1c0b335ab7edc
edumoritz <eduardo.moritz@hotmail.com>
2021-03-03 21:55:43 -0300

    comunication with firebase

--- Http/http-01-start/src/app/app.component.ts
+++ Http/http-01-start/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+const urlFirebase = 'https://ng-complete-guide-3f8c9-default-rtdb.firebaseio.com';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,13 +11,16 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent implements OnInit {
   loadedPosts = [];
 
+
   constructor(private http: HttpClient) {}
 
   ngOnInit() {}
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
-    console.log(postData);
+    this.http.post(urlFirebase+'/posts.json', postData).subscribe(responseData => {
+      console.log(responseData)
+    });
   }
 
   onFetchPosts() {
