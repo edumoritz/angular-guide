e4aa8764293563e667be641b8c39b94e9b88e085
edumoritz <eduardo.moritz@hotmail.com>
2021-04-01 22:51:34 -0300

    validation error in service

--- shopping-thebasic/src/app/auth/auth.service.ts
+++ shopping-thebasic/src/app/auth/auth.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 const endpoint = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyB_1yEjRbImYj2Zs6Xs4k3FK_zQu_xfY64';
 
@@ -18,6 +20,18 @@ export class AuthService {
   constructor(private http: HttpClient) { }
 
   signup(email: string, password: string) {
-    return this.http.post(endpoint, { email, password, returnSecureToken: true })
+    return this.http.post(
+      endpoint, { email, password, returnSecureToken: true }
+    ).pipe(catchError(errorRes => {
+      let errorMessage = 'An unknown error occurred!';
+      if (!errorRes.error || !errorRes.error.error) {
+        return throwError(errorMessage);
+      }
+      switch (errorRes.error.error.message) {
+        case 'EMAIL_EXISTS':
+          errorMessage = 'This email exists already';
+      }
+      return throwError(errorMessage);
+    }));
   }
 }
