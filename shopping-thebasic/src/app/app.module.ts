4df0ad4b2e5196ced1617d5734c5aca708334af3
edumoritz <eduardo.moritz@hotmail.com>
2021-04-15 22:23:34 -0300

    implementing lazy loading

--- shopping-thebasic/src/app/app.module.ts
+++ shopping-thebasic/src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AuthModule } from './auth/auth.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
-import { RecipesModule } from './recipes/recipes.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -23,7 +22,6 @@ import { HeaderComponent } from './header/header.component';
     CoreModule,
     AppRoutingModule,
     AuthModule,
-    RecipesModule,
     ShoppingListModule,
   ],
   bootstrap: [AppComponent],
