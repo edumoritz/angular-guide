aec970b1810ea1059abeae7fb539636a5abf6a5d
edumoritz <eduardo.moritz@hotmail.com>
2021-05-23 11:55:25 -0300

    Login via ngrx

--- shopping-thebasic-ngrx/src/app/app.module.ts
+++ shopping-thebasic-ngrx/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { AuthEffects } from './auth/store/auth.effects';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
@@ -11,6 +12,7 @@ import { HeaderComponent } from './header/header.component';
 import { shoppingListReducer } from './shopping-list/store/shopping-list.reducer';
 import { authReducer } from './auth/store/auth.reducer';
 import * as fromApp from './store/app.reducer';
+import { EffectsModule } from '@ngrx/effects';
 
 @NgModule({
   declarations: [
@@ -22,6 +24,7 @@ import * as fromApp from './store/app.reducer';
     BrowserModule,
     AppRoutingModule,
     StoreModule.forRoot(fromApp.appReducer),
+    EffectsModule.forRoot([AuthEffects]),
     SharedModule,
     CoreModule,
   ],
--- shopping-thebasic-ngrx/src/app/auth/auth.component.ts
+++ shopping-thebasic-ngrx/src/app/auth/auth.component.ts
@@ -1,11 +1,14 @@
-import { PlaceHolderDirective } from './../shared/placeholder/placeholder.directive';
+import { Store } from '@ngrx/store';
 import { Router } from '@angular/router';
 import { AuthService, AuthResponseData } from './auth.service';
 import { NgForm } from '@angular/forms';
 import { Component, ComponentFactoryResolver, OnInit, ViewChild, OnDestroy } from '@angular/core';
 import { Observable, Subscription } from 'rxjs';
 
+import { PlaceHolderDirective } from './../shared/placeholder/placeholder.directive';
 import { AlertComponent } from '../shared/alert/alert.component';
+import * as fromApp from '../store/app.reducer';
+import * as AuthActions from './store/auth.actions';
 
 @Component({
   selector: 'app-auth',
@@ -24,8 +27,15 @@ export class AuthComponent implements OnInit, OnDestroy {
     private authService: AuthService,
     private router: Router,
     private componentFactoryResolver: ComponentFactoryResolver,
+    private store: Store<fromApp.AppState>
   ) { }
-  ngOnInit() { }
+
+  ngOnInit() {
+    this.store.select('auth').subscribe(authState => {
+      this.isLoading = authState.loading;
+      this.error = authState.authError;
+    });
+  }
 
   onSwitchMode() {
     this.isLoginMode = !this.isLoginMode;
@@ -42,21 +52,26 @@ export class AuthComponent implements OnInit, OnDestroy {
 
     this.isLoading = true;
     if (this.isLoginMode) {
-      authObs = this.authService.login(email, password)
+      // authObs = this.authService.login(email, password)
+      this.store.dispatch(
+        new AuthActions.LoginStart({ email, password })
+      );
     } else {
       authObs = this.authService.signup(email, password)
     }
 
-    authObs.subscribe(resData => {
-      console.log(resData);
-      this.isLoading = false;
-      this.router.navigate(['/recipes']);
-    }, errorMessage => {
-      console.log(errorMessage);
-      this.error = errorMessage;
-      this.showErrorAlert(errorMessage);
-      this.isLoading = false;
-    });
+
+
+    // authObs.subscribe(resData => {
+    //   console.log(resData);
+    //   this.isLoading = false;
+    //   this.router.navigate(['/recipes']);
+    // }, errorMessage => {
+    //   console.log(errorMessage);
+    //   this.error = errorMessage;
+    //   this.showErrorAlert(errorMessage);
+    //   this.isLoading = false;
+    // });
 
     form.reset();
   }
